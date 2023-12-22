e3b8c589797997404a76ffc03e1ef75b1d88d1ca
djdobro <nikitaivanov838@gmail.com>
2023-12-23 00:13:12 +0300

    conflict resolve

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -65,7 +65,6 @@ public class UserDaoJDBCImpl extends Util implements UserDao {
                 user.setId(resultSet.getLong("id"));
                 user.setName(resultSet.getString("name"));
                 user.setLastName(resultSet.getString("lastname"));
-                //Комментарий измененный
                 user.setAge(resultSet.getByte("age"));
                 allUsers.add(user);
             }
